e3be18757a7d0e923d83b421e963c5dde85c3460
Niger <nigerabed@gmail.com>
2023-09-28 12:18:36 +0200

    add eventlistener

--- script.js
+++ script.js
@@ -9,22 +9,28 @@ function addItem(card) {
   container.setAttribute("class", "container");
   box.appendChild(container);
 
-  container.innerHTML = `
-    <div class="card">
-        <div class="front">     
-        </div> 
-        <div class="back"> 
-            <h3>${card.tag}</h3>
-            <br>
-            type: ${card.type} 
-            <br>
-            attributes:${card.attributes[(0, 1)]}
-            description: ${card.description}
-        </div> 
-    </div>
+  let div = document.createElement("div")
+  div.setAttribute("class", "card") ;
+  div.innerHTML = `
+    <div class="front"></div> 
+    <div class="back"> 
+        <h3>${card.tag}</h3>
+        <br>
+        type: ${card.type} 
+        <br>
+        attributes:${card.attributes[(0, 1)]}
+        description: ${card.description}
+    </div> 
   `;
+  container.appendChild(div)
+  div.addEventListener("click", function handleClick(event){
+    div.classList.toggle("flipped")
+  })
 }
 
 for (let i = 0; i < 12; i++) {
   addItem(data[i]);
 }
+
+
+
